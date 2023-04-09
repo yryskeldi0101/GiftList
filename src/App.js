748fdc370bb7c5ea6558ea57f05dbb03d76db7cf
Naz-Erke18 <bektemirovanika72@gmail.com>
2023-04-10 00:48:26 +0600

    meatballs

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ const arrayIcon = [
    {
       icon: KeyIcon,
       title: 'Заблокировать',
+      id: '1',
    },
    {
       icon: Anonim,
       title: 'Заблокировать анонимно',
+      id: '2',
    },
 ]
 function App() {
--- src/components/UI/Meatballs.jsx
+++ src/components/UI/Meatballs.jsx
@@ -1,38 +1,39 @@
 import { Button, Menu, MenuItem } from '@mui/material'
 import { ReactComponent as MeatballsIcon } from '../../assets/icons/meatballs.svg'
-
-export default function Meatballs({ arrayIcon, open, handleClick, anchorEl, handleClose }) {
-
-
+export default function Meatballs({ ...restProps }) {
     return (
         <div>
             <Button
                 id="demo-positioned-button"
-                aria-controls={open ? "demo-positioned-menu" : undefined}
+                aria-controls={restProps.open ? 'demo-positioned-menu' : undefined}
                 aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
+                aria-expanded={restProps.open ? 'true' : undefined}
+                onClick={restProps.handleClick}
             >
                 <MeatballsIcon />
             </Button>
             <Menu
                 id="demo-positioned-menu"
                 aria-labelledby="demo-positioned-button"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
+                anchorEl={restProps.anchorEl}
+                open={restProps.open}
+                onClose={restProps.handleClose}
                 anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
+                    vertical: 'top',
+                    horizontal: 'left',
                 }}
                 transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
+                    vertical: 'top',
+                    horizontal: 'left',
                 }}
             >
-                {arrayIcon.map((item) => (
-                    <MenuItem onClick={handleClose}>
-                        <img src={item.icon} alt="#" style={{ marginRight: '10px' }} />
+                {restProps.arrayIcon.map((item) => (
+                    <MenuItem onClick={restProps.handleClose} key={item.id}>
+                        <img
+                            src={item.icon}
+                            alt="#"
+                            style={{ marginRight: '10px' }}
+                        />
                         {item.title}
                     </MenuItem>
                 ))}
