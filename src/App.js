1316ed722729c80a0ddc54243557e5d89940c113
Naz-Erke18 <bektemirovanika72@gmail.com>
2023-04-10 11:24:07 +0600

    meatballs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React from 'react'
-import Meatballs from './components/UI/Meatballs'
-import Anonim from './assets/icons/anonim.svg'
-import KeyIcon from './assets/icons/key.svg'
-import { useMeatballs } from './hooks/useMeatballs'
 
-const arrayIcon = [
-   {
-      icon: KeyIcon,
-      title: 'Заблокировать',
-      id: '1',
-   },
-   {
-      icon: Anonim,
-      title: 'Заблокировать анонимно',
-      id: '2',
-   },
-]
 function App() {
-   const { open, handleClick, anchorEl, handleClose } = useMeatballs()
-
-   return (
-      <div className="App">
-         <Meatballs
-            arrayIcon={arrayIcon}
-            open={open}
-            handleClick={handleClick}
-            anchorEl={anchorEl}
-            handleClose={handleClose}
-         />
-      </div>
-   )
+   return <div />
 }
 
 export default App
--- src/components/UI/Meatballs.jsx
+++ src/components/UI/Meatballs.jsx
@@ -1,43 +1,53 @@
 import { Button, Menu, MenuItem } from '@mui/material'
 import { ReactComponent as MeatballsIcon } from '../../assets/icons/meatballs.svg'
-export default function Meatballs({ ...restProps }) {
-    return (
-        <div>
-            <Button
-                id="demo-positioned-button"
-                aria-controls={restProps.open ? 'demo-positioned-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={restProps.open ? 'true' : undefined}
-                onClick={restProps.handleClick}
-            >
-                <MeatballsIcon />
-            </Button>
-            <Menu
-                id="demo-positioned-menu"
-                aria-labelledby="demo-positioned-button"
-                anchorEl={restProps.anchorEl}
-                open={restProps.open}
-                onClose={restProps.handleClose}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-            >
-                {restProps.arrayIcon.map((item) => (
-                    <MenuItem onClick={restProps.handleClose} key={item.id}>
-                        <img
-                            src={item.icon}
-                            alt="#"
-                            style={{ marginRight: '10px' }}
-                        />
-                        {item.title}
-                    </MenuItem>
-                ))}
-            </Menu>
-        </div>
-    )
+
+export default function Meatballs({
+   arrayIcon,
+   open,
+   handleClose,
+   handleClick,
+   anchorEl,
+   ...restProps
+}) {
+   return (
+      <div>
+         <Button
+            id="demo-positioned-button"
+            aria-controls={open ? 'demo-positioned-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            {...restProps}
+         >
+            <MeatballsIcon />
+         </Button>
+         <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+               vertical: 'top',
+               horizontal: 'left',
+            }}
+            transformOrigin={{
+               vertical: 'top',
+               horizontal: 'left',
+            }}
+            {...restProps}
+         >
+            {arrayIcon.map((item) => (
+               <MenuItem onClick={handleClose} key={item.id} {...restProps}>
+                  <img
+                     src={item.icon}
+                     alt="#"
+                     style={{ marginRight: '10px' }}
+                  />
+                  {item.title}
+               </MenuItem>
+            ))}
+         </Menu>
+      </div>
+   )
 }
