f1bebad3fcdfbc93afd69b4c01da8389f8d986df
Saltanat <zakypovasaltanat6@gmail.com>
2023-04-11 18:40:26 +0600

    Replaced a modal with a frame-based modal

--- src/components/UI/modal/Modal.jsx
+++ src/components/UI/modal/Modal.jsx
@@ -1,39 +1,9 @@
-import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
 
-export default function Modal({
-   open,
-   handleClose,
-   title,
-   message,
-   agreeText,
-   disagreeText,
-}) {
+export default function Modal({ open, handleClose, children, ...rest }) {
    return (
-      <Dialog
-         open={open}
-         onClose={handleClose}
-         aria-labelledby="alert-dialog-title"
-         aria-describedby="alert-dialog-description"
-      >
-         <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-
-         <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-               {message}
-            </DialogContentText>
-         </DialogContent>
-
-         <DialogActions>
-            <Button onClick={handleClose}>{disagreeText}</Button>
-            <Button onClick={handleClose} autoFocus>
-               {agreeText}
-            </Button>
-         </DialogActions>
+      <Dialog open={open} onClose={handleClose} {...rest}>
+         {children}
       </Dialog>
    )
 }
