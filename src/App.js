54b4a06bfaa02b1842c93e25a54958ab81510bd2
Begimai7 <begimai.keldibekova.kk@gmail.com>
2023-04-12 12:20:11 +0600

    create checkbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Checkboxes from './components/UI/Checkbox'
 
 function App() {
    return <div />
---
+++ src/components/UI/Checkbox.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Checkbox from '@mui/material/Checkbox'
+import { styled } from '@mui/material'
+
+const StyledCheckbox = styled(Checkbox)(() => ({
+   color: '#8639B5;',
+   '&.Mui-checked': {
+      color: '#8639B5;',
+   },
+}))
+
+export default function Checkboxes() {
+   return <StyledCheckbox />
+}
