ab1fe5c73552f63c32bac628b1f81603ad2ee450
Naz-Erke18 <bektemirovanika72@gmail.com>
2023-04-12 22:29:28 +0600

    detailedPage

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-         <App />
-          </ThemeProvider>
+         <ThemeProvider theme={theme}>
+            <App />
+         </ThemeProvider>
       </BrowserRouter>
    </React.StrictMode>
 )
