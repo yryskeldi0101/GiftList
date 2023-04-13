64ca99f7ccd70f490eefcd03938606f61731a5f0
aielina5 <elinaisabekova457@gmail.com>
2023-04-13 21:00:40 +0600

    feature/SnackBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import MySnackbar from './components/button/SnackBar'
+
 function App() {
-   return <div />
+   return (
+      <div>
+         <MySnackbar title="Информация" message="Текст сообщения" type="info" />
+      </div>
+   )
 }
 
 export default App
---
+++ src/components/button/SnackBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function MySnackbar({ message, title, type }) {
+   const toastConfig = {
+      position: 'top-left',
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: false,
+   }
+
+   const messageStyle = {
+      color: 'green',
+      position: 'relative',
+      top: '10px',
+      left: '10px',
+   }
+   const titleStyle = {
+      color: 'black',
+      position: 'relative',
+      top: '19px',
+      left: '10px',
+   }
+   const notify = () => {
+      toast[type](
+         <>
+            <h4 style={titleStyle}>{title}</h4>
+            <p style={messageStyle}>{message}</p>
+         </>,
+         toastConfig
+      )
+   }
+
+   return (
+      <div>
+         <button type="button" onClick={notify}>
+            Toast
+         </button>
+         <ToastContainer />
+      </div>
+   )
+}
+
+export default MySnackbar
