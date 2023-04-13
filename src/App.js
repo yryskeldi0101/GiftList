721f3daaa3d33af49c01a2fe3f3ad8dbd871a343
FrontenderO07 <nurlanbekovich2006@gmail.com>
2023-04-13 14:35:39 +0600

    clean app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import FiveSection from './components/landing-page/FiveSection'
-import ThrirdSection from './components/landing-page/ThrirdSection'
-import FourSection from './components/landing-page/FourSection'
-import Footer from './components/landing-page/Footer'
 
 function App() {
-   return (
-      <div>
-         <ThrirdSection />
-         <FourSection />
-         <FiveSection />
-         <Footer />
-      </div>
-   )
+   return <div />
 }
 
 export default App
