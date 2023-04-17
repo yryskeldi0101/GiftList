5554b42fea8de707149723b332c0b20aa880cbdb
aielina5 <elinaisabekova457@gmail.com>
2023-04-17 12:50:25 +0600

    fixed/RadioButton

--- src/components/button/RadioButton.jsx
+++ src/components/button/RadioButton.jsx
@@ -1,41 +1,29 @@
 import React from 'react'
+import Radio from '@mui/material/Radio'
 
-const RadioButton = ({ label, name, value, checked, onChange }) => (
-   <label style={{ display: 'flex', alignItems: 'center' }}>
-      <input
-         type="radio"
-         name={name}
-         value={value}
-         checked={checked}
-         onChange={onChange}
-         style={{ display: 'none' }}
-      />
-      <div
-         style={{
-            width: '20px',
-            height: '20px',
-            borderRadius: '50%',
-            border: `2px solid ${checked ? 'blue' : 'lightgray'}`,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginRight: '10px',
-            background: `${checked ? 'blue' : 'white'}`,
-         }}
-      >
-         {checked && (
-            <div
-               style={{
-                  width: '10px',
-                  height: '10px',
-                  borderRadius: '50%',
-                  background: 'white',
-               }}
-            />
-         )}
+export default function RadioButtons() {
+   const [selectedValue, setSelectedValue] = React.useState('a')
+
+   const handleChange = (event) => {
+      setSelectedValue(event.target.value)
+   }
+
+   return (
+      <div>
+         <Radio
+            checked={selectedValue === 'a'}
+            onChange={handleChange}
+            value="a"
+            name="radio-buttons"
+            inputProps={{ 'aria-label': 'A' }}
+         />
+         <Radio
+            checked={selectedValue === 'b'}
+            onChange={handleChange}
+            value="b"
+            name="radio-buttons"
+            inputProps={{ 'aria-label': 'B' }}
+         />
       </div>
-      {label}
-   </label>
-)
-
-export default RadioButton
+   )
+}
