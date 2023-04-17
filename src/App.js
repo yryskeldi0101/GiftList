ed4af18fbc49ada0618182919b3d170ffc5af2bf
Saltanat <zakypovasaltanat6@gmail.com>
2023-04-17 17:28:30 +0600

    fixed App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { LandingPage } from './containers/LandingPage'
-import EmptyTextarea from './components/UI/textarea/Textarea'
 
 function App() {
    return (
       <div>
          <LandingPage />
-         <EmptyTextarea />
       </div>
    )
 }
