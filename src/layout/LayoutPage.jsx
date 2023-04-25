02791b99ffb27816002760d29e2ffcb32cc11102
Beishenbekov2003 <beishenbekov.010103@gmail.com>
2023-04-26 00:02:05 +0600

    LayoutPage with semantic tags

--- src/layout/LayoutPage.jsx
+++ src/layout/LayoutPage.jsx
@@ -6,18 +6,25 @@ import Header from './Header'
 
 const LayoutPage = () => {
    return (
-      <main>
-         <Header />
-         <SideBar />
-         <Container>
+      <>
+         <HeaderContent>
+            <Header />
+         </HeaderContent>
+         <aside>
+            <SideBar />
+         </aside>
+         <MainContainer>
             <Outlet />
-         </Container>
-      </main>
+         </MainContainer>
+      </>
    )
 }
 
 export default LayoutPage
 
-const Container = styled('div')`
+const HeaderContent = styled('header')`
+   margin-left: 332px;
+`
+const MainContainer = styled('main')`
    margin-left: 332px;
 `
