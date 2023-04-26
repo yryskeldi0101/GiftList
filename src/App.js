81c9ddddd3071364a786ce63c3733362acb52a4d
FrontenderO07 <nurlanbekovich2006@gmail.com>
2023-04-26 15:28:08 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import Header from './components/Header'
 
 function App() {
-   return (
-      <div>
-         <Header />
-      </div>
-   )
+   return <div />
 }
 export default App
