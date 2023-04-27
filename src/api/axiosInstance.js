2a40f506e74fb0e1fb734a9ecf997881190714a5
aielina5 <elinaisabekova457@gmail.com>
2023-04-27 18:57:14 +0600

    fixed/axiosInstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,25 +1,31 @@
 import axios from 'axios'
 
-const store = 0
 const BASE_ULR = 'http://ec2-3-121-207-55.eu-central-1.compute.amazonaws.com'
 export const axiosInstance = axios.create({ baseURL: BASE_ULR })
 
 axiosInstance.interceptors.request.use(
-   function (config) {
-      config.headers.set('Authorization', store.getState().auth.token)
-      return config
+   (config) => {
+      const configureStore = { ...config }
+      const {
+         auth: { token },
+      } = store.getState()
+      if (token) {
+         configureStore.headers.Authorization = `Bearer ${token}`
+      }
+      return configureStore
    },
-   function (error) {
+   (error) => {
       return Promise.reject(error)
    }
 )
+
 axiosInstance.interceptors.response.use(
    function responsees(response) {
       return response
    },
    function cathError(error) {
       if (error.response.status === 401) {
-         store.dispatch()
+         throw new Error('Error')
       }
       return Promise.reject(error)
    }
